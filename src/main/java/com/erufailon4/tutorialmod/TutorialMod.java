04812568df33867fca682ba3dc66e7b977f9e5b5
Erufailon4 <erufailon4@gmail.com>
2018-09-15 12:48:40 +0300

    Changed "RiftTest" to "TutorialMod"

--- src/main/java/com/erufailon4/tutorialmod/TutorialMod.java
+++ src/main/java/com/erufailon4/tutorialmod/TutorialMod.java
@@ -1,9 +1,9 @@
-package com.erufailon4.rifttest;
+package com.erufailon4.tutorialmod;
 
 import net.minecraft.item.*;
 import org.dimdev.rift.listener.ItemAdder;
 
-public class RiftTest implements ItemAdder {
+public class TutorialMod implements ItemAdder {
 
     public static final Item COPPER_INGOT = new Item(new Item.Builder().group(ItemGroup.MATERIALS));
 
