e20232bd3a9a4524b88987525c6e59383a6e24fc
Erufailon4 <erufailon4@gmail.com>
2018-09-21 19:50:04 +0300

    Added basic mashed potato

--- src/main/java/com/erufailon4/tutorialmod/TutorialMod.java
+++ src/main/java/com/erufailon4/tutorialmod/TutorialMod.java
@@ -10,12 +10,15 @@ import org.dimdev.rift.listener.ItemAdder;
 public class TutorialMod implements ItemAdder, BlockAdder {
 
     public static final Item COPPER_INGOT = new Item(new Item.Builder().group(ItemGroup.MATERIALS));
+    public static final Item MASHED_POTATO = new ItemFood(2, 1, false, new Item.Builder().group(ItemGroup.FOOD));
+    // new ItemFood(healAmount, saturationModifier, isWolfsFavoriteMeat, super)
 
     public static final Block COPPER_ORE = new Block(Block.Builder.create(Material.ROCK, MapColor.STONE).hardnessAndResistance(3.0F, 3.0F));
 
     @Override
     public void registerItems() {
         Item.registerItem("tutorialmod:copper_ingot", COPPER_INGOT);
+        Item.registerItem("tutorialmod:mashed_potato", MASHED_POTATO);
         Item.register(COPPER_ORE, ItemGroup.BUILDING_BLOCKS);
     }
 
